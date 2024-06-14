624d57e2146cf51afa6ce1a0bdc63bdf3ae42f1b
Daniel <daniel.albert.liu@gmail.com>
2024-06-14 15:33:23 -0500

    updated telescope mappings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,4 @@
-return {
+jeturn({
 	-- Adds git related signs to the gutter, as well as utilities for managing changes
 	{
 		"lewis6991/gitsigns.nvim",
@@ -127,17 +127,19 @@ return {
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "[S]earch [B]uffers" })
 			vim.keymap.set(
 				"n",
 				"<leader>/",
 				builtin.current_buffer_fuzzy_find,
 				{ desc = "[/] Fuzzily search in current buffer" }
 			)
-			vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "[S]earch [B]uffers" })
 		end,
 	},
-}
+})
