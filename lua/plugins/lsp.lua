8bdd1db76adbb55af4314826c1e23a9d4142b669
Daniel <daniel.albert.liu@gmail.com>
2024-06-14 15:46:25 -0500

    more lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,13 +30,34 @@ return {
 					end
 
 					-- Go to definition of a variable
-					map("gd", vim.lsp.buf.definition, "[G]o to [D]efinition")
+					map("gd", vim.lsp.buf.definition, "[G]o to [d]efinition")
 
 					-- Go to references of a variable
 					map("gr", vim.lsp.buf.references, "[G]o to [R]eferences")
 
+					-- Jump to the implementation of the word under your cursor.
+					map("gi", vim.lsp.buf.implementation, "[G]o to [I]mplementation")
+
+					-- Jump to the type definition of the word under your cursor.
+					map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+
+					-- Show the symbols in the current document.
+					map("<leader>ds", vim.lsp.buf.document_symbol, "[D]ocument [S]ymbol")
+
+					-- Show the symbols in the current workspace.
+					map("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [S]ymbol")
+
 					-- Rename the variable under your cursor.
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+					-- Execute a code action
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+					-- Opens a popup that displays documentation about the word under your cursor
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+					-- Go to declaration
+					map("gD", vim.lsp.buf.declaration, "[G]o to [D]eclaration")
 				end,
 			})
 
