d8633d079403e4752b454df4e56a341f0ad3a3dd
Daniel <daniel.albert.liu@gmail.com>
2024-06-14 15:36:05 -0500

    fixed typo

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,4 @@
-jeturn({
+return {
 	-- Adds git related signs to the gutter, as well as utilities for managing changes
 	{
 		"lewis6991/gitsigns.nvim",
@@ -142,4 +142,4 @@ jeturn({
 			)
 		end,
 	},
-})
+}
