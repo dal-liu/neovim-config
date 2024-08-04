e6bd92cbf0ca5e8428dbd94800bd48e4d269fdd3
Daniel <daniel.albert.liu@gmail.com>
2024-08-03 22:35:59 -0500

    colorscheme and which key

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -8,6 +8,7 @@ return {
 	-- Useful plugin to show you pending keybinds.
 	{
 		"folke/which-key.nvim",
+		tag = "v2.1.0",
 		event = "VimEnter",
 		dependencies = {
 			"echasnovski/mini.icons",
@@ -17,13 +18,13 @@ return {
 
 			-- Document existing key chains
 			require("which-key").add({
-				{ "<leader>c", group = "[C]ode" },
-				{ "<leader>d", group = "[D]ocument" },
-				{ "<leader>r", group = "[R]ename" },
-				{ "<leader>s", group = "[S]earch" },
-				{ "<leader>w", group = "[W]orkspace" },
-				{ "<leader>t", group = "[T]oggle" },
-				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
+				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
 			})
 		end,
 	},
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,11 @@
 return {
-	-- One Dark Pro
-	{
-		"olimorris/onedarkpro.nvim",
-		tag = "0.8.0",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		init = function()
-			-- Load the colorscheme here.
-			vim.cmd.colorscheme("onedark")
-		end,
-		config = function()
-			local colors = require("onedarkpro.helpers").get_colors()
-			require("onedarkpro").setup({
-				highlights = {
-					["@comment"] = { fg = colors.comment, italic = true },
-					CursorLineNr = { fg = colors.white, extend = true },
-					NvimTreeFolderName = { fg = colors.white },
-					NvimTreeRootFolder = { fg = colors.comment },
-					NvimTreeFolderIcon = { fg = colors.blue },
-					NvimTreeOpenedFolderName = { fg = colors.blue },
-				},
-				options = {
-					cursorline = true,
-				},
-			})
-		end,
-	},
-
 	-- Ayu
 	{
 		"Shatur/neovim-ayu",
 		priority = 1000,
 		init = function()
 			-- Load the colorscheme here.
-			-- vim.cmd.colorscheme("ayu")
+			vim.cmd.colorscheme("ayu")
 		end,
 		config = function()
 			require("ayu").setup({
